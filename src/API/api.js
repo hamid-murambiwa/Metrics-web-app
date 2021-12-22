e9e045a19bdae701d4e5ba956d5d5cbe0a4e8cbe
hamid-murambiwa <machipisajunior@gmail.com>
2021-12-22 22:18:09 +0200

    Made the following changes: Install linters files fix all eslint errors Add styling folder Add styling to the home page and details page Update README.md file

--- src/API/api.js
+++ src/API/api.js
@@ -5,27 +5,27 @@ const users = () => async (dispatch) => {
     headers: {
       'x-api-key': 'c478d8f2-a659-4ef4-83b8-a0735ee45add',
       'Pagination-Count': 20,
-    }
+    },
   });
-    let arr = [];
-    books.data.forEach((item) => arr.push({
-      title: item.name,
-      image: item.image.url,
-      group: item.breed_group,
-      id: item.id,
-      origin: item.origin,
-      life_span: item.life_span,
-      bred_for: item.bred_for,
-      height_imperial: `imperial: ${item.height.imperial}`,
-      height_metric: `metric: ${item.height.metric}`,
-      weight_imperial: `imperial: ${item.weight.imperial}`,
-      weight_metric: `metric: ${item.weight.metric}`,
-      temperament: item.temperament,
-    }));
-    dispatch({
-      type: "artStore/api/GET_RECORDS",
-      arr,
-    });
-}
+  const arr = [];
+  books.data.forEach((item) => arr.push({
+    title: item.name,
+    image: item.image.url,
+    group: item.breed_group,
+    id: item.id,
+    origin: item.origin,
+    life_span: item.life_span,
+    bred_for: item.bred_for,
+    height_imperial: `imperial: ${item.height.imperial}`,
+    height_metric: `metric: ${item.height.metric}`,
+    weight_imperial: `imperial: ${item.weight.imperial}`,
+    weight_metric: `metric: ${item.weight.metric}`,
+    temperament: item.temperament,
+  }));
+  dispatch({
+    type: 'artStore/api/GET_RECORDS',
+    arr,
+  });
+};
 
 export default users;
--- src/pages/details.js
+++ src/pages/details.js
@@ -1,109 +1,111 @@
-import  React, { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import users from '../API/api';
 import DetailsHeader from '../component/detailsHeader';
+import '../styling/details.css';
 
 function Details() {
   const detailsList = useSelector((state) => state.artReducer);
   const dispatch = useDispatch();
-  // let details = [detailsList[window.location.pathname.slice(1)]];
 
   useEffect(() => {
     dispatch(users());
-}, [dispatch]);
+  }, [dispatch]);
 
-let image;
-let name;
-let bred_for;
-let breed_group;
-let height_metric;
-let height_imperial;
-let weight_metric;
-let weight_imperial;
-let origin;
-let temperament;
+  let image;
+  let name;
+  let bredFor;
+  let breedGroup;
+  let heightMetric;
+  let heightImperial;
+  let weightMetric;
+  let weightImperial;
+  let origin;
+  let temperament;
 
-detailsList.map((cases) => {
-  if (cases.id == window.location.pathname.slice(1)) {
-    // console.log(cases);
-    image = cases.image;
-    name = cases.title;
-    bred_for = cases.bred_for;
-    breed_group = cases.group;
-    height_metric = cases.height_metric;
-    height_imperial = cases.height_imperial;
-    weight_metric = cases.weight_metric;
-    weight_imperial = cases.weight_imperial;
-    origin = cases.origin;
-    temperament = cases.temperament;
-  }
-});
+  detailsList.map((cases) => {
+    if (cases.id == window.location.pathname.slice(1)) {
+      image = cases.image;
+      name = cases.title;
+      bredFor = cases.bred_for;
+      breedGroup = cases.group;
+      heightMetric = cases.height_metric;
+      heightImperial = cases.height_imperial;
+      weightMetric = cases.weight_metric;
+      weightImperial = cases.weight_imperial;
+      origin = cases.origin;
+      temperament = cases.temperament;
+    }
+  });
 
- console.log(origin);
+  console.log(origin);
 
-
-return (
-    <div key={1}>
+  return (
+    <div>
       <div>
-        <DetailsHeader />
+        <DetailsHeader name={name} />
       </div>
-      <h1>
-      {name}
-      </h1>
-      <img src={image} alt='' />
-      <ul>
-        <li>
-          <h2>
-            Breed group:
-          </h2>
-        <p>
-        {breed_group}
-      </p>
-        </li>
+      <section id="d-con">
+        <div id="i-con">
+          <img src={image} id="breed-img" alt="" />
+          <h1>
+            {name}
+          </h1>
+        </div>
+        <ul className="details">
+          <li>
+            <h2>
+              Breed group
+            </h2>
+            <p>
+              {breedGroup}
+            </p>
+          </li>
 
-        <li>
-          <h2>
-            Breed for:
-          </h2>
-      <p>
-      {bred_for}
-      </p>
-        </li>
+          <li>
+            <h2>
+              Breed for
+            </h2>
+            <p>
+              {bredFor}
+            </p>
+          </li>
 
-        <li>
-          <h2>Country origin:</h2>
-      <p>
-       {origin}
-      </p>
-        </li>
+          <li>
+            <h2>Country origin</h2>
+            <p>
+              {origin}
+            </p>
+          </li>
 
-        <li>
-          <h2>Characteristics:</h2>
-      <p>
-        {temperament}
-      </p>
-      </li>
+          <li>
+            <h2>Characteristics</h2>
+            <p>
+              {temperament}
+            </p>
+          </li>
 
-      <li>
-        <h2>Height:</h2>
-      <p>
-        {height_imperial}
-      </p>
-      <p>
-      {height_metric}
-      </p>
-      </li>
+          <li>
+            <h2>Height</h2>
+            <p>
+              {heightImperial}
+            </p>
+            <p>
+              {heightMetric}
+            </p>
+          </li>
 
-      <li>
-        <h2>Weight:</h2>
-        <p>
-        {weight_imperial}
-      </p>
-      <p>
-      {weight_metric}
-      </p>
-      </li>
-      </ul>
+          <li>
+            <h2>Weight</h2>
+            <p>
+              {weightImperial}
+            </p>
+            <p>
+              {weightMetric}
+            </p>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
